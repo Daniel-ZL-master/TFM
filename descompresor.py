6e5b450ba1a98a9052b6068035cb50351d909530
Daniel <danielzorlej@gmail.com>
2026-02-04 11:06:38 +0100

    import neurokit

--- descompresor.py
+++ descompresor.py
@@ -8,3 +8,5 @@ Created on Mon Feb 2 12:51:03 2026
 TO-DO: Crear el código del descompresor que reciba la información del receptor y la
 procese para extraer los picos y enviarlos a un archivo o base de datos.
 """
+
+import neurokit2 as nk
